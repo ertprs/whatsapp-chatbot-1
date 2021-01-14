1236845d12d143d49b7e18ef44fed062c4f3532f
Sérgio Cardoso <scardoso@lidercap.com.br>
2021-01-14 09:06:38 -0300

    change

--- src/twiliio.js
+++ src/twiliio.js
@@ -1,10 +1,28 @@
-const twilio = require('twilio')
+const twilio = require('twilio');
+const MessagingResponse = require('twilio/lib/twiml/MessagingResponse');
 
 const accountSid = process.env.SID
 const authToken = process.env.KEY
 
 const client = twilio(accountSid, authToken)
-const messagingResponse = twilio.twiml.MessagingResponse;
+const messagingResponse = new twilio.twiml.MessagingResponse;
 
 exports.client = client
 exports.messagingResponse = messagingResponse
+
+exports.sendMessage = (message) => {
+
+    const twiml = new MessagingResponse();
+
+    console.log('retornando', req.body.Body);
+
+    if (req.body.Body.toLowerCase().trim() !== "Ola") {
+
+    }
+
+    client.messages.create({
+        body: message,
+        from: `whatsapp:${process.env.TWILIO_PHONENUMBER}`,
+        to: 
+    })
+}
